5089c986c010f1c12005d0b3223c0fa6e88e127c
Елизавета Носкова <wliza68@gmail.com>
2023-07-16 15:51:31 +0300

    first commit

---
+++ src/main/java/com/example/cwauction2/CwAuction2Application.java
@@ -0,0 +1,13 @@
+package com.example.cwauction2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwAuction2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwAuction2Application.class, args);
+    }
+
+}
